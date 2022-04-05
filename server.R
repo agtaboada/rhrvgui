bfe230ce32e9daa8293f1e1ac59921d593b459b9
Aitor <agtaboada@esei.uvigo.es>
2022-04-05 14:17:45 +0200

    Funcionalidad basica menu heart rate data
    Incluida opción para cargar ficheros ASCII
    Incluida opción para filtrar datos
    Incluida opción para la edición de datos

--- server.R
+++ server.R
@@ -1,3 +1,40 @@
-server <- function(input, output){
-
-}
+shinyServer(function(input, output, session){
+    print("Nueva ejecucion")
+    hrv.data <- NULL
+    volumes <- c(Home = fs::path_home(), "R Installation" = R.home(), getVolumes()())
+    shinyFileChoose(input, "loadHrButton", roots = volumes, session = session)
+    hrv.data = reactiveVal()
+    hrv.data = CreateHRVData()
+    hrv.data = SetVerbose(hrv.data, TRUE)
+    observeEvent(input$loadHrButton, { 
+      file <- reactive(input$loadHrButton)
+      if(length(file()) > 0 & is.numeric(file())){
+        return(NULL)
+      }else{
+        datapath <- parseFilePaths(volumes, file())$datapath
+        datapath <- gsub("/","\\\\", datapath)
+        datapath <- gsub(basename(datapath), "", datapath)
+        hrv.data = LoadBeatAscii(hrv.data, parseFilePaths(volumes, file())$name, datapath)
+        hrv.data = BuildNIHR(hrv.data)
+        hrv.data <<- hrv.data
+        output$mainGraph<-renderPlot({
+          PlotNIHR(hrv.data)
+          })
+        print(hrv.data)
+      }
+    })
+    observeEvent(input$filterHrButton,{
+      hrv.data = FilterNIHR(hrv.data)
+      hrv.data<<- hrv.data
+      output$mainGraph<-renderPlot({
+        PlotNIHR(hrv.data, main="Filtered non-interpolated heart rate")
+        })
+    })
+    
+    observeEvent(input$editHrButton, {
+        hrv.data = EditNIHR(hrv.data)
+        hrv.data <<- hrv.data
+        output$mainGraph<-renderPlot({
+          PlotNIHR(hrv.data, main="Edited data")})
+    })
+  })
--- ui.R
+++ ui.R
@@ -1,4 +1,58 @@
 library(RHRV)
 library(shiny)
+library(shinyFiles)
+library(shinyjs)
+library(spsComps)
+library(tkrplot)
 ui<-fluidPage(
+  tags$link(rel = "stylesheet", type="text/css", href="css/style.css"),
+  navbarPage("RHRV GUI",
+    tabPanel("Main menu"),
+    tabPanel("Interpolate"),
+    tabPanel("Frame-based evolution"),
+    tabPanel("Report"),
+    tabPanel("Pointcare plot")
+  ),
+  sidebarPanel("",
+               fluidRow(
+                 h3("Options")
+               ),
+               fluidRow(
+                 h4("Heart rate data")
+               ),
+               fluidRow(
+                 shinyFilesButton("loadHrButton",
+                            "Load data",
+                            "Select a file",
+                            FALSE,
+                            buttonType = "default",
+                            class = NULL,
+                            icon = NULL,
+                            style = NULL,
+                            viewtype = "detail",
+                 ),
+                 actionButton("filterHrButton", "Filter"),
+                 actionButton("editHrButton", "Edit")
+               ),
+               fluidRow(
+                 h4("Episodes")
+               ),
+                actionButton("loadEpButton", "Load"),
+                actionButton("clearEpButton", "Clear"),
+                actionButton("editEpButton", "Edit")
+               ),
+  mainPanel("",
+              fluidRow(
+                plotOutput("mainGraph",
+                           width = "100%",
+                           height = "400px",
+                           click = NULL,
+                           dblclick = NULL,
+                           hover = NULL,
+                           brush = NULL,
+                           inline = FALSE
+                )
+              ),
+              useShinyjs()
+  )
 )
