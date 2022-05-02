f37c3a441c04d68bac3b51acfd0f31a5262504cc
Aitor <agtaboada@esei.uvigo.es>
2022-05-02 20:31:14 +0200

    Modificada navegacion para mostrar nueva ventana

--- ui.R
+++ ui.R
@@ -5,60 +5,68 @@ library(shinyjs)
 library(spsComps)
 library(tkrplot)
 ui<-fluidPage(
+  useShinyjs(),
   tags$link(rel = "stylesheet", type="text/css", href="css/style.css"),
   navbarPage("RHRV GUI",
-    tabPanel("Main menu"),
+    tabPanel("Main menu",
+             sidebarPanel(id="mainSidebar",
+                          "",
+                          fluidRow(
+                            h3("Options")
+                          ),
+                          fluidRow(
+                            h4("Heart rate data")
+                          ),
+                          fluidRow(
+                            shinyFilesButton("loadHrButton",
+                                             "Load data",
+                                             "Select a file",
+                                             FALSE,
+                                             buttonType = "default",
+                                             class = NULL,
+                                             icon = NULL,
+                                             style = NULL,
+                                             viewtype = "detail",
+                            ),
+                            actionButton("filterHrButton", "Filter"),
+                            actionButton("editHrButton", "Edit")
+                          ),
+                          fluidRow(
+                            h4("Episodes")
+                          ),
+                          shinyFilesButton("loadEpButton", "Load episodes",
+                                           "Select a file",
+                                           FALSE,
+                                           buttonType = "default",
+                                           class = NULL,
+                                           icon = NULL,
+                                           style = NULL,
+                                           viewtype = "detail"),
+                          actionButton("clearEpButton", "Clear")
+             ),
+             mainPanel(id="mainMainPanel",
+                       "",
+                       fluidRow(
+                         plotOutput("mainGraph",
+                                    width = "100%",
+                                    height = "400px",
+                                    click = NULL,
+                                    dblclick = NULL,
+                                    hover = NULL,
+                                    brush = NULL,
+                                    inline = FALSE
+                         )
+                       )
+             )         
+    ),
     tabPanel("Interpolate"),
     tabPanel("Frame-based evolution"),
     tabPanel("Report"),
-    tabPanel("Pointcare plot")
-  ),
-  sidebarPanel("",
-               fluidRow(
-                 h3("Options")
-               ),
-               fluidRow(
-                 h4("Heart rate data")
-               ),
-               fluidRow(
-                 shinyFilesButton("loadHrButton",
-                            "Load data",
-                            "Select a file",
-                            FALSE,
-                            buttonType = "default",
-                            class = NULL,
-                            icon = NULL,
-                            style = NULL,
-                            viewtype = "detail",
-                 ),
-                 actionButton("filterHrButton", "Filter"),
-                 actionButton("editHrButton", "Edit")
-               ),
-               fluidRow(
-                 h4("Episodes")
-               ),
-                shinyFilesButton("loadEpButton", "Load episodes",
-                                 "Select a file",
-                                 FALSE,
-                                 buttonType = "default",
-                                 class = NULL,
-                                 icon = NULL,
-                                 style = NULL,
-                                 viewtype = "detail"),
-                actionButton("clearEpButton", "Clear")
-               ),
-  mainPanel("",
-              fluidRow(
-                plotOutput("mainGraph",
-                           width = "100%",
-                           height = "400px",
-                           click = NULL,
-                           dblclick = NULL,
-                           hover = NULL,
-                           brush = NULL,
-                           inline = FALSE
-                )
-              ),
-              useShinyjs()
+    tabPanel(id="panelPoincare","Poincare plot",
+             fluidPage(
+               sidebarPanel(id="poincareSidebar",
+               h4("Poincare Plot")
+                            )
+             )
   )
-)
+))
