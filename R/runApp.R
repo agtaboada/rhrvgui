fdf5521214499372c670ae8faf769407bc147a5d
agtaboada <agtaboada@esei.uvigo.es>
2022-07-13 20:19:53 +0200

    Modificaciones para creacion de paquete R

---
+++ R/runApp.R
@@ -0,0 +1,10 @@
+#' runApplication
+#'
+#' Starts the application.
+#' @return Opens the GUI on a new window.
+#' @examples 
+#' runApplication()
+#' @export
+runApplication <- function(){
+  shiny::runApp("R")
+}
